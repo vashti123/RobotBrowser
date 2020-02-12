cc8dd7041f55f869cdbae84632735277585a21ac
vashti123 <31171100+vashti123@users.noreply.github.com>
2020-02-12 10:23:11 -0500

    Add files via upload

---
+++ navJS.js
@@ -0,0 +1,16 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+function myFunction() {
+  var x = document.getElementById("myBottomnav");
+  if (x.className === "bottomnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "bottomnav";
+  }
+}
