6b288c8079da67a80e82cebf198d8b82a5790166
vashti123 <31171100+vashti123@users.noreply.github.com>
2020-03-06 08:44:02 -0500

    Added tutorial pages, HTML, PDF, and DOCX. Reformatted the Robots, and Prints pages to contain the correct information. Added imgs folder, contains all images for webpage.

---
+++ session.js
@@ -0,0 +1,9 @@
+function uncheck(target) {
+  target.innerHTML = '<img id="box" src="img/blackBox.png" width="10px" height="10px"/>';
+  target.setAttribute('onclick', 'check(this)')
+}
+
+function check(target) {
+  target.innerHTML = '<img id="box" src="img/checkedBox.png" width="10px" height="10px"/>';
+  target.setAttribute('onclick', 'uncheck(this)')
+}
